0ca4af09d0c0ff80a6c3cc99879c46eea19484f0
SantiEjercicios <redondosanti50@gmail.com>
2025-11-21 14:12:05 +0100

    Creo las balsas e inicio los hilos

--- src/Rescate.java
+++ src/Rescate.java
@@ -1,8 +1,37 @@
-public class Rescate implements Runnable{
+public class Rescate {
+    private Barco barco;
 
-    public Rescate(){}
-    @Override
-    public void run() {
-        while(true){}
+    public Rescate() {
+        this.barco = new Barco();
+    }
+
+    public void comenzarRescate() {
+        System.out.println("!!! COMIENZA EL OPERATIVO DE RESCATE CON HILOS !!!");
+
+        Balsa b1 = new Balsa("Acasta", 1, 10.5, barco);
+        Balsa b2 = new Balsa("Banff", 2, 21, barco);
+        Balsa b3 = new Balsa("Cadiz", 3, 32, barco);
+        Balsa b4 = new Balsa("Deimos", 4, 44, barco);
+        Balsa b5 = new Balsa("Expedición", 5, 58, barco);
+
+
+        b1.start();
+        b2.start();
+        b3.start();
+        b4.start();
+        b5.start();
+
+
+        try {
+            b1.join();
+            b2.join();
+            b3.join();
+            b4.join();
+            b5.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("--- RESCATE COMPLETADO ---");
     }
 }
