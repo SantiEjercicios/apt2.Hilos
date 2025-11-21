a4eeaf327de7d11e2ed1c3d53f241b2fabd0d37b
SantiEjercicios <redondosanti50@gmail.com>
2025-11-21 14:09:49 +0100

    Elimino la categoria, la veia innecesaria

--- src/Barco.java
+++ src/Barco.java
@@ -16,14 +16,8 @@ public class Barco {
         Random rand = new Random();
 
         for (int i = 1; i <= 352; i++) {
-            int tipo = rand.nextInt(4) + 1; // 1 a 4
-            String cat = "";
-            switch (tipo) {
-                case 1 -> cat = "Niño";
-                case 2 -> cat = "Adulto";
-                case 3 -> cat = "Anciano";
-                default -> cat = "Tripulación"; // 4
-            }
+            int tipo = rand.nextInt(4) + 1;
+
 
             pasajeros.add(new Pasajero("P-"+i,tipo));
 
