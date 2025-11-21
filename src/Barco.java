98385a310a8016409641a54d28a2a0768531e857
SantiEjercicios <redondosanti50@gmail.com>
2025-11-21 14:09:07 +0100

    Genero los pasajeros y les añado el tipo de prioridad

--- src/Barco.java
+++ src/Barco.java
@@ -1,16 +1,45 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 public class Barco {
+    private List<Pasajero> pasajeros;
 
-    private final List<Pasajero> pasajeros = new ArrayList<>();
-
-    public Barco(List<Pasajero> pasajeros) {
-        this.pasajeros.addAll(pasajeros);
+    public Barco() {
+        this.pasajeros = new ArrayList<>();
+        generarTripulacion();
+        ordenarPorPrioridad();
     }
 
-    public boolean hayPasajeros(){
-        return !pasajeros.isEmpty();
+    private void generarTripulacion() {
+        Random rand = new Random();
+
+        for (int i = 1; i <= 352; i++) {
+            int tipo = rand.nextInt(4) + 1; // 1 a 4
+            String cat = "";
+            switch (tipo) {
+                case 1 -> cat = "Niño";
+                case 2 -> cat = "Adulto";
+                case 3 -> cat = "Anciano";
+                default -> cat = "Tripulación"; // 4
+            }
+
+            pasajeros.add(new Pasajero("P-"+i,tipo));
+
+        }
     }
 
+    private void ordenarPorPrioridad() {
+
+        pasajeros.sort(Comparator.comparingInt(Pasajero::getPrioridad));
+        System.out.println("--- BARCO: Pasajeros ordenados por prioridad ---");
+    }
+
+
+    public synchronized Pasajero obtenerPasajero() {
+        if (!pasajeros.isEmpty()) {
+            return pasajeros.remove(0);
+        }
+        return null; }
 }
