61f326e45c2cb21a4df03607e43e274d0b423303
SantiEjercicios <redondosanti50@gmail.com>
2025-12-04 12:16:19 +0100

    implementamos el uso de semaforos

--- src/Barco.java
+++ src/Barco.java
@@ -2,9 +2,12 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Semaphore; // IMPORTANTE
 
 public class Barco {
     private List<Pasajero> pasajeros;
+
+    private Semaphore semaforo = new Semaphore(1, true);
 
     public Barco() {
         this.pasajeros = new ArrayList<>();
@@ -17,23 +20,40 @@ public class Barco {
 
         for (int i = 1; i <= 352; i++) {
             int tipo = rand.nextInt(4) + 1;
-
-
-            pasajeros.add(new Pasajero("P-"+i,tipo));
-
+            String cat = switch (tipo) {
+                case 1 -> "Niño";
+                case 2 -> "Adulto";
+                case 3 -> "Anciano";
+                default -> "Tripulación";
+            };
+            pasajeros.add(new Pasajero("P-" + i, tipo));
         }
     }
 
     private void ordenarPorPrioridad() {
-
         pasajeros.sort(Comparator.comparingInt(Pasajero::getPrioridad));
-        System.out.println("--- BARCO: Pasajeros ordenados por prioridad ---");
+        System.out.println("--- BARCO: Pasajeros ordenados y listos (Protegido por SEMÁFORO) ---");
     }
 
 
-    public synchronized Pasajero obtenerPasajero() {
-        if (!pasajeros.isEmpty()) {
-            return pasajeros.remove(0);
+    public Pasajero obtenerPasajero() {
+        Pasajero p = null;
+        try {
+
+            semaforo.acquire();
+
+
+            if (!pasajeros.isEmpty()) {
+                p = pasajeros.remove(0);
+            }
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+
+            semaforo.release();
         }
-        return null; }
+        return p;
+    }
 }
